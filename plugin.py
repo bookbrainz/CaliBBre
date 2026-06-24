7382d289ed2ab92072afca69949893673d090c3a
Md Waqib Sk <rentusk.1970@gmail.com>
2026-06-24 20:42:55 +0530

    add: main plugin class

--- plugin.py
+++ plugin.py
@@ -8,3 +8,27 @@ class BookBrainzPlugin(QtWidgets.QDialog, Ui_Dialog):
     Main plugin class,
     Inherits from QDialog for the popup window and Ui_Dialog for all the UI widgets
     """
+
+    def __init__(self, gui):
+        super(BookBrainzPlugin, self).__init__()
+
+        self.gui = gui
+        self.setupUi(self)
+
+        # Wire up the action buttons
+        self.pushButton_fetch_metadataTab.clicked.connect(self.fetch_metadata)
+        self.pushButton_update_metadataTab.clicked.connect(self.update_metadata)
+
+        self.tabWidget.setCurrentIndex(0)
+        self.stackedWidget_metadataDetails_metadataTab.setCurrentIndex(0)
+        selected_ids = self.gui.library_view.get_selected_ids()
+
+        db = self.gui.current_db.new_api
+        if not selected_ids:
+            # No book selected -> show the "No Book Selected" placeholder
+            self.stackedWidget_entire_metadataTab.setCurrentIndex(1)
+            return
+        else:
+            first_book_id = selected_ids[0]
+            self.mi = db.get_metadata(first_book_id)
+            self.search_book()
