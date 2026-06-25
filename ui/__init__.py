8b2a86b8cae4945df117a627bb115cda2e53b190
Md Waqib Sk <rentusk.1970@gmail.com>
2026-06-25 20:01:21 +0530

    fix: remove unused stackedwidget

--- ui/__init__.py
+++ ui/__init__.py
@@ -80,7 +80,6 @@ class Ui_Dialog(MetadataTabMixin, BrowseTabMixin, CollectionTabMixin):
         self.stackedWidget_entire_metadataTab.setCurrentIndex(0)
         self.stackedWidget_searchResults_metadataTab.setCurrentIndex(0)
         self.stackedWidget_metadataDetails_metadataTab.setCurrentIndex(1)
-        self.stackedWidget.setCurrentIndex(0)
 
         self._update_tab_widths()
         QtCore.QMetaObject.connectSlotsByName(Dialog)
